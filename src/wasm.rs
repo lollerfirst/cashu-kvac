91ba94fb56726fc8eac43df1b8d7f1b2f68635ca
lollerfirst <lollerfirst@gmail.com>
2025-03-20 16:31:29 +0100

    fixes

--- src/wasm.rs
+++ src/wasm.rs
@@ -16,59 +16,68 @@ use crate::{
 
 #[wasm_bindgen]
 impl MintPrivateKey {
-    pub fn wasm_from_scalars(scalars: Vec<Scalar>) -> Result<Self, JsValue> {
+    #[allow(non_snake_case)]
+    pub fn wasmFromScalars(scalars: Vec<Scalar>) -> Result<Self, JsValue> {
         Self::from_scalars(&scalars).map_err(|e| JsValue::from_str(&format!("{}", e)))
     }
 
-    pub fn wasm_to_scalars(&self) -> Vec<Scalar> {
+    #[allow(non_snake_case)]
+    pub fn wasmToScalars(&self) -> Vec<Scalar> {
         self.to_scalars()
     }
 }
 
 #[wasm_bindgen]
 impl AmountAttribute {
-    pub fn wasm_create_new(amount: u64, blinding_factor: Option<Vec<u8>>) -> Self {
-        match blinding_factor {
+    #[allow(non_snake_case)]
+    pub fn wasmCreateNew(amount: u64, blindingFactor: Option<Vec<u8>>) -> Self {
+        match blindingFactor {
             Some(blinding_factor) => Self::new(amount, Some(&blinding_factor)),
             None => Self::new(amount, None),
         }
     }
 
-    pub fn wasm_commitment(&self) -> GroupElement {
+    #[allow(non_snake_case)]
+    pub fn wasmCommitment(&self) -> GroupElement {
         self.commitment()
     }
 
-    pub fn wasm_tweak_amount(&mut self, amount: u64) {
+    #[allow(non_snake_case)]
+    pub fn wasmTweakAmount(&mut self, amount: u64) {
         let _ = self.tweak_amount(amount);
     }
 }
 
 #[wasm_bindgen]
 impl ScriptAttribute {
-    pub fn wasm_create_new(script: Vec<u8>, blinding_factor: Option<Vec<u8>>) -> Self {
-        match blinding_factor {
+
+    #[allow(non_snake_case)]
+    pub fn wasmCreateNew(script: Vec<u8>, blindingFactor: Option<Vec<u8>>) -> Self {
+        match blindingFactor {
             Some(blinding_factor) => Self::new(&script, Some(&blinding_factor)),
             None => Self::new(&script, None),
         }
     }
 
-    pub fn wasm_commitment(&self) -> GroupElement {
+    #[allow(non_snake_case)]
+    pub fn wasmCommitment(&self) -> GroupElement {
         self.commitment()
     }
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 impl MAC {
-    pub fn wasm_generate(
-        mint_privkey: &MintPrivateKey,
-        amount_commitment: GroupElement,
-        script_commitment: Option<GroupElement>,
+    pub fn wasmGenerate(
+        mintPrivkey: &MintPrivateKey,
+        amountCommitment: GroupElement,
+        scriptCommitment: Option<GroupElement>,
         tag: Option<Scalar>,
     ) -> Result<Self, JsValue> {
         Self::generate(
-            mint_privkey,
-            &amount_commitment,
-            script_commitment.as_ref(),
+            mintPrivkey,
+            &amountCommitment,
+            scriptCommitment.as_ref(),
             tag.as_ref(),
         )
         .map_err(|e| JsValue::from_str(&format!("{}", e)))
@@ -76,64 +85,72 @@ impl MAC {
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 impl Coin {
-    pub fn wasm_create_new(
-        amount_attribute: AmountAttribute,
-        script_attribute: Option<ScriptAttribute>,
+    pub fn wasmCreateNew(
+        amountAttribute: AmountAttribute,
+        scriptAttribute: Option<ScriptAttribute>,
         mac: MAC,
     ) -> Self {
-        Self::new(amount_attribute, script_attribute, mac)
+        Self::new(amountAttribute, scriptAttribute, mac)
     }
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 impl RandomizedCoin {
-    pub fn wasm_from_coin(coin: &Coin, reveal_script: bool) -> Result<Self, JsValue> {
-        Self::from_coin(coin, reveal_script).map_err(|e| JsValue::from_str(&format!("{}", e)))
+    pub fn wasmFromCoin(coin: &Coin, revealScript: bool) -> Result<Self, JsValue> {
+        Self::from_coin(coin, revealScript).map_err(|e| JsValue::from_str(&format!("{}", e)))
     }
 }
 
 #[wasm_bindgen]
 impl BootstrapProof {
-    pub fn wasm_create(
-        amount_attribute: &AmountAttribute,
+
+    #[allow(non_snake_case)]
+    pub fn wasmCreate(
+        amountAttribute: &AmountAttribute,
         transcript: &mut CashuTranscript,
     ) -> ZKP {
-        BootstrapProof::create(amount_attribute, transcript)
+        BootstrapProof::create(amountAttribute, transcript)
     }
 
-    pub fn wasm_verify(
-        amount_commitment: &GroupElement,
+    #[allow(non_snake_case)]
+    pub fn wasmVerify(
+        amountCommitment: &GroupElement,
         proof: ZKP,
         transcript: &mut CashuTranscript,
     ) -> bool {
-        BootstrapProof::verify(amount_commitment, proof, transcript)
+        BootstrapProof::verify(amountCommitment, proof, transcript)
     }
 }
 
 #[wasm_bindgen]
 impl MacProof {
-    pub fn wasm_create(
-        mint_publickey: &MintPublicKey,
+
+    #[allow(non_snake_case)]
+    pub fn wasmCreate(
+        mintPublickey: &MintPublicKey,
         coin: &Coin,
-        randomized_coin: &RandomizedCoin,
+        randomizedCoin: &RandomizedCoin,
         transcript: &mut CashuTranscript,
     ) -> ZKP {
-        MacProof::create(mint_publickey, coin, randomized_coin, transcript)
+        MacProof::create(mintPublickey, coin, randomizedCoin, transcript)
     }
 
-    pub fn wasm_verify(
-        mint_privkey: &MintPrivateKey,
-        randomized_coin: &RandomizedCoin,
+    #[allow(non_snake_case)]
+    pub fn wasmVerify(
+        mintPrivkey: &MintPrivateKey,
+        randomizedCoin: &RandomizedCoin,
         script: Option<Vec<u8>>,
         proof: ZKP,
         transcript: &mut CashuTranscript,
     ) -> bool {
         match script {
-            None => MacProof::verify(mint_privkey, randomized_coin, None, proof, transcript),
+            None => MacProof::verify(mintPrivkey, randomizedCoin, None, proof, transcript),
             Some(script) => MacProof::verify(
-                mint_privkey,
-                randomized_coin,
+                mintPrivkey,
+                randomizedCoin,
                 Some(&script),
                 proof,
                 transcript,
@@ -143,36 +160,41 @@ impl MacProof {
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 impl IParamsProof {
-    pub fn wasm_create(
-        mint_privkey: &MintPrivateKey,
+
+    #[allow(non_snake_case)]
+    pub fn wasmCreate(
+        mintPrivkey: &MintPrivateKey,
         mac: &MAC,
-        amount_commitment: GroupElement,
-        script_commitment: Option<GroupElement>,
+        amountCommitment: GroupElement,
+        scriptCommitment: Option<GroupElement>,
         transcript: &mut CashuTranscript,
     ) -> ZKP {
         IParamsProof::create(
-            mint_privkey,
+            mintPrivkey,
             mac,
-            &amount_commitment,
-            script_commitment.as_ref(),
+            &amountCommitment,
+            scriptCommitment.as_ref(),
             transcript,
         )
     }
 
-    pub fn wasm_verify(
-        mint_publickey: &MintPublicKey,
+    #[allow(non_snake_case)]
+    pub fn wasmVerify(
+        mintPublickey: &MintPublicKey,
         coin: &Coin,
         proof: ZKP,
         transcript: &mut CashuTranscript,
     ) -> bool {
-        IParamsProof::verify(mint_publickey, coin, proof, transcript)
+        IParamsProof::verify(mintPublickey, coin, proof, transcript)
     }
 }
 
 #[wasm_bindgen]
 impl BalanceProof {
-    pub fn wasm_create(
+    #[allow(non_snake_case)]
+    pub fn wasmCreate(
         inputs: Vec<AmountAttribute>,
         outputs: Vec<AmountAttribute>,
         transcript: &mut CashuTranscript,
@@ -180,63 +202,70 @@ impl BalanceProof {
         BalanceProof::create(&inputs, &outputs, transcript)
     }
 
-    pub fn wasm_verify(
+    #[allow(non_snake_case)]
+    pub fn wasmVerify(
         inputs: Vec<RandomizedCoin>,
         outputs: Vec<GroupElement>,
-        delta_amount: i64,
+        deltaAmount: i64,
         proof: ZKP,
         transcript: &mut CashuTranscript,
     ) -> bool {
-        BalanceProof::verify(&inputs, &outputs, delta_amount, proof, transcript)
+        BalanceProof::verify(&inputs, &outputs, deltaAmount, proof, transcript)
     }
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 pub struct OutputAttributesPair {
-    amount_attribute: AmountAttribute,
-    script_attribute: ScriptAttribute,
+    amountAttribute: AmountAttribute,
+    scriptAttribute: ScriptAttribute,
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 pub struct OutputCommitmentsPair {
-    amount_commitment: GroupElement,
-    script_commitment: GroupElement,
+    amountCommitment: GroupElement,
+    scriptCommitment: GroupElement,
 }
 
 #[wasm_bindgen]
 impl ScriptEqualityProof {
-    pub fn wasm_create(
+
+    #[allow(non_snake_case)]
+    pub fn wasmCreate(
         inputs: Vec<Coin>,
-        randomized_inputs: Vec<RandomizedCoin>,
+        randomizedInputs: Vec<RandomizedCoin>,
         outputs: Vec<OutputAttributesPair>,
         transcript: &mut CashuTranscript,
     ) -> Result<ZKP, JsValue> {
         let outputs: Vec<(AmountAttribute, ScriptAttribute)> = outputs
             .into_iter()
-            .map(|o| (o.amount_attribute, o.script_attribute))
+            .map(|o| (o.amountAttribute, o.scriptAttribute))
             .collect();
-        ScriptEqualityProof::create(&inputs, &randomized_inputs, &outputs, transcript)
+        ScriptEqualityProof::create(&inputs, &randomizedInputs, &outputs, transcript)
             .map_err(|e| JsValue::from_str(&format!("{}", e)))
     }
 
-    pub fn wasm_verify(
-        randomized_inputs: Vec<RandomizedCoin>,
+    #[allow(non_snake_case)]
+    pub fn wasmVerify(
+        randomizedInputs: Vec<RandomizedCoin>,
         outputs: Vec<OutputCommitmentsPair>,
         proof: ZKP,
         transcript: &mut CashuTranscript,
     ) -> bool {
         let outputs: Vec<(GroupElement, GroupElement)> = outputs
             .into_iter()
-            .map(|o| (o.amount_commitment, o.script_commitment))
+            .map(|o| (o.amountCommitment, o.scriptCommitment))
             .collect();
-        ScriptEqualityProof::verify(&randomized_inputs, &outputs, proof, transcript)
+        ScriptEqualityProof::verify(&randomizedInputs, &outputs, proof, transcript)
     }
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 pub struct WasmInnerProductArgument {
-    public_inputs: JsValue,
-    tail_end_scalars: (Scalar, Scalar),
+    publicInputs: JsValue,
+    tailEndScalars: (Scalar, Scalar),
 }
 
 #[wasm_bindgen]
@@ -246,58 +275,69 @@ pub struct WasmBulletProof {
     S: GroupElement,
     T1: GroupElement,
     T2: GroupElement,
-    t_x: Scalar,
-    tau_x: Scalar,
+    tX: Scalar,
+    tauX: Scalar,
     mu: Scalar,
     ipa: WasmInnerProductArgument,
 }
 
 #[wasm_bindgen]
 impl WasmBulletProof {
-    pub fn wasm_create(attributes: Vec<AmountAttribute>, transcript: &mut CashuTranscript) -> Self {
+    #[allow(non_snake_case)]
+    pub fn wasmCreate(attributes: Vec<AmountAttribute>, transcript: &mut CashuTranscript) -> Self {
         let bulletproof = BulletProof::new(transcript, &attributes);
-        let wasm_ipa = WasmInnerProductArgument {
-            public_inputs: to_value(&bulletproof.ipa.public_inputs)
+        let wasmIpa = WasmInnerProductArgument {
+            publicInputs: to_value(&bulletproof.ipa.public_inputs)
                 .expect("can always convert IPA public inputs to JsValue"),
-            tail_end_scalars: bulletproof.ipa.tail_end_scalars,
+            tailEndScalars: bulletproof.ipa.tail_end_scalars,
         };
         WasmBulletProof {
             A: bulletproof.A,
             S: bulletproof.S,
             T1: bulletproof.T1,
             T2: bulletproof.T2,
-            t_x: bulletproof.t_x,
-            tau_x: bulletproof.t_x,
+            tX: bulletproof.t_x,
+            tauX: bulletproof.tau_x,
             mu: bulletproof.mu,
-            ipa: wasm_ipa,
+            ipa: wasmIpa,
         }
     }
 
-    pub fn wasm_verify(
+    #[allow(non_snake_case)]
+    pub fn wasmVerify(
         self,
-        attribute_commitments: Vec<GroupElement>,
+        attributeCommitments: Vec<GroupElement>,
         transcript: &mut CashuTranscript,
     ) -> bool {
-        let ipa_public_inputs: Result<Vec<(GroupElement, GroupElement)>, Error> =
-            from_value(self.ipa.public_inputs);
-        let ipa_public_inputs = match ipa_public_inputs {
-            Ok(ipa_public_inputs) => ipa_public_inputs, // If the conversion is successful, return the public inputs
-            Err(_) => return false,                     // If conversion fails, return false
+    let ipaPublicInputs: Result<Vec<(GroupElement, GroupElement)>, Error> =
+        from_value(self.ipa.publicInputs);
+        let ipaPublicInputs = match ipaPublicInputs {
+            Ok(ipaPublicInputs) => ipaPublicInputs, // If the conversion is successful, return the public inputs
+            Err(_) => return false,                  // If conversion fails, return false
         };
         let ipa = InnerProductArgument {
-            public_inputs: ipa_public_inputs,
-            tail_end_scalars: self.ipa.tail_end_scalars,
+            public_inputs: ipaPublicInputs,
+            tail_end_scalars: self.ipa.tailEndScalars,
         };
         let bulletproof = BulletProof {
             A: self.A,
             S: self.S,
             T1: self.T1,
             T2: self.T2,
-            t_x: self.t_x,
-            tau_x: self.tau_x,
+            t_x: self.tX,
+            tau_x: self.tauX,
             mu: self.mu,
             ipa,
         };
-        bulletproof.verify(transcript, &attribute_commitments)
+        bulletproof.verify(transcript, &attributeCommitments)
     }
 }
+    
+#[wasm_bindgen]
+#[allow(non_snake_case)]
+impl CashuTranscript {
+    pub fn wasmCreateNew() -> Self {
+        Self::new()
+    }
+}
+    
